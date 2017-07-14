961f7923bd5a6459d32cb79eb199a691643d7937
René Löscher <reneloescher@gmx.net>
2017-07-14 23:08:08 +0200

    added character configuration

---
+++ Assets/Scripts/Configs/CharacterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "CharacterConfiguration", menuName = "Configurations/CharacterConfiguration")]
+public class CharacterConfig : ScriptableObject
+{
+    [SerializeField] public float JumpHeight;
+    [SerializeField] public float MovementSpeed;
+    [SerializeField] public float TimeToJumpMax;
+    [SerializeField] public float AccelerationTimeGrounded;
+    [SerializeField] public float AccelerationTimeAir;
+}
