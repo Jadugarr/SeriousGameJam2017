b07b07938b0b5d025bcadf020c7681eb9e101ab5
SirObvious <pascal.worreschk@gmail.com>
2017-07-15 19:30:54 +0200

    TileChanges

--- Assets/Scripts/PickupScripts/Pill_SpawnBlock.cs
+++ Assets/Scripts/PickupScripts/Pill_SpawnBlock.cs
@@ -6,24 +6,30 @@ public class Pill_SpawnBlock : MonoBehaviour {
 
     public GameObject hiddenBlock;
     //Private Stuff
-    private SpriteRenderer hiddenBlockSR;
+    private SpriteRenderer[] hiddenBlockSR;
     private float lerpAcc = 0f;
     private Color startColor;
 
 	// Use this for initialization
 	void Start () {
-        hiddenBlockSR = hiddenBlock.GetComponent<SpriteRenderer>();
-        startColor = new Color(hiddenBlockSR.color.r, hiddenBlockSR.color.g, hiddenBlockSR.color.b, hiddenBlockSR.color.a);
+        hiddenBlockSR = hiddenBlock.GetComponentsInChildren<SpriteRenderer>();
+        startColor = new Color(1f, 1f, 1f, 0.01f);
+        for(int i = 0; i < hiddenBlockSR.Length; i++)
+        {
+            hiddenBlockSR[i].color = startColor;
+        }
         hiddenBlock.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(hiddenBlock.activeSelf && hiddenBlockSR.color.a < 0.99f)
+		if(hiddenBlock.activeSelf && hiddenBlockSR[0].color.a < 0.99f)
         {
             lerpAcc += Time.deltaTime;
-            hiddenBlockSR.color = Color.Lerp(startColor, new Color(1f, 1f, 1f, 1f), lerpAcc);
-            Debug.Log("LERP");
+            for (int i = 0; i < hiddenBlockSR.Length; i++)
+            {
+                hiddenBlockSR[i].color = Color.Lerp(startColor, new Color(1f, 1f, 1f, 1f), lerpAcc);
+            }
         }
 	}
 
