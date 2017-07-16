e76b9dfffe87fee8949db3c2b5d9419bb82b28b3
r0bzilla <r0bzilla@gmx.net>
2017-07-16 12:54:33 +0200

    PUUUUURGE MOTHERFUCKER

---
+++ Assets/EndingScript.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingScript : MonoBehaviour {
+	[SerializeField]
+	protected Animator player;
+
+	[SerializeField]
+	protected CanvasGroup fadeGroup;
+
+	[SerializeField]
+	protected float startTime;
+
+	[SerializeField]
+	protected float playerTargetX;
+
+	[SerializeField]
+	protected float walkSpeed;
+
+	[SerializeField]
+	protected float cameraTargetY;
+
+	[SerializeField]
+	protected float cameraSpeed;
+
+	[SerializeField]
+	protected Transform cameraTransform;
+
+	[SerializeField]
+	protected float fadeOutTime;
+
+
+	private int phase = 0;
+	float startTimestamp;
+	float endTimestamp;
+
+	// Use this for initialization
+	void Awake () {
+		startTimestamp = Time.realtimeSinceStartup;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float timePassed =  Time.realtimeSinceStartup - startTimestamp;
+
+		if(phase == 0 && timePassed >= startTime)
+		{
+			phase = 1;
+			startWalking();
+		}
+		else if( phase == 1)
+		{
+			walking();
+		}
+		else if( phase == 3)
+		{
+			cameraMove();
+		}
+		else if( phase == 4)
+		{
+			fadeOut();
+		}
+	}
+
+	protected void startWalking()
+	{
+		player.SetTrigger("walk");
+	}
+
+	protected void walking()
+	{
+		Vector3 pos = player.transform.position;
+		if(pos.x < playerTargetX)
+		{
+			pos.x += walkSpeed;
+			player.transform.position = pos;
+		}
+		else
+		{
+			startSexyTime();
+		}
+	}
+
+	protected void startSexyTime()
+	{
+		phase = 2;
+		player.SetTrigger("strip");
+	}
+
+	protected void finishSexyTime()
+	{
+		phase = 3;
+	}
+
+	protected void cameraMove()
+	{
+		Vector3 pos = cameraTransform.position;
+		if(pos.y < cameraTargetY)
+		{
+			pos.y += cameraSpeed;
+			cameraTransform.position = pos;
+		}
+		else
+		{
+			theEnd();
+		}
+	}
+
+	protected void theEnd()
+	{
+		phase = 4;
+		endTimestamp = Time.realtimeSinceStartup;
+	}
+
+	protected void fadeOut()
+	{
+		float progress = Mathf.Clamp((Time.realtimeSinceStartup - endTimestamp) / fadeOutTime, 0f, 1f);
+
+		if( progress == 1.0f )
+		{
+			fadeGroup.alpha = 1f;
+
+			phase = 5;
+			SceneManager.LoadScene( 0);
+		}
+		else
+		{
+			fadeGroup.alpha = progress;
+		}
+	}
+}
---
+++ Assets/FadeInScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInScript : MonoBehaviour {
+
+	protected CanvasGroup fadeGroup;
+
+	[SerializeField]
+	protected float fadeDuration = 0.6f;
+
+	[SerializeField]
+	protected AudioSource fadeAudio = null;
+
+	protected float fadeStartTimeStamp;
+	protected float maxVol;
+
+	// Use this for initialization
+	void Awake () {
+		fadeStartTimeStamp = Time.realtimeSinceStartup;
+		fadeGroup = GetComponent<CanvasGroup>();
+		if(fadeAudio != null)
+		{
+			maxVol = fadeAudio.volume;
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float progress = Mathf.Clamp((Time.realtimeSinceStartup - fadeStartTimeStamp) / fadeDuration, 0f, 1f);
+		if(progress == 1f)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			fadeGroup.alpha = 1f - progress;
+			if(fadeAudio != null)
+			{
+				fadeAudio.volume = maxVol * progress;
+			}
+		}
+	}
+}
