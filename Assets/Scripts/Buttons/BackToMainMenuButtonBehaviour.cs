021e108da32131e1b4c638560dcc290ee170f396
René Löscher <reneloescher@gmx.net>
2017-07-16 06:57:34 +0200

    Added main menu scene

---
+++ Assets/Scripts/Buttons/BackToMainMenuButtonBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BackToMainMenuButtonBehaviour : MonoBehaviour
+{
+    private Button button;
+
+    // Use this for initialization
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnButtonClick);
+    }
+
+    void OnDestroy()
+    {
+        button.onClick.RemoveListener(OnButtonClick);
+    }
+
+    private void OnButtonClick()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
